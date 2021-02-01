a76f79fb720c22ecfb21229250f0441d13f76f1a
Hamid.haghshenas <h.hagshenas@hotmail.com>
2021-02-01 21:23:19 +0330

    master

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+my_dict={
+   "name":"hamid",
+   "family":"hg",
+   "age":36
+
+}
+print(my_dict)
+print(type(my_dict))
+print(my_dict["name"])
+print(my_dict.get("age"))
+print(my_dict)
+my_dict["name"]="ali"
+my_dict["age"]=30
+print(my_dict)
