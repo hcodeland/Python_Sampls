2365997750f12b2ec69df5578db0e2446632d989
Hamid.haghshenas <h.hagshenas@hotmail.com>
2021-02-05 15:35:47 +0330

    set

---
+++ set.py
@@ -0,0 +1,12 @@
+my_set={'title','author','content','subject'}
+print(type(my_set))
+print(my_set)
+
+my_set.add("title")
+print(my_set)
+
+my_set.add("year")
+print(my_set)
+
+my_set.update(["1",10,12.5,"Word"])
+print(my_set)
